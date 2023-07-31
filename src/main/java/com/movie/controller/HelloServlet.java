0e55d72f1f69981932ff50bb464e57dd0c5e6cef
LuongTuanVy <ltvy910@gmail.com>
2023-07-31 22:59:07 +0700

    Fix bug xong roi

--- src/main/java/com/movie/controller/HelloServlet.java
+++ src/main/java/com/movie/controller/HelloServlet.java
@@ -1,9 +1,21 @@
 package com.movie.controller;
 
+import com.movie.entity.User;
 import com.movie.service.UserService;
 import com.movie.service.implement.UserServiceImplement;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -13,23 +25,20 @@ import javax.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
 
 
-    private UserService userService;
+    private UserService userService = new UserServiceImplement();
+
+    private SessionFactory sessionFactory;
 
     @Override
     public void init() throws ServletException {
-        userService = new UserServiceImplement();
+        super.init();
+        sessionFactory = (SessionFactory) getServletContext().getAttribute("sessionFactory");
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/admin/home.jsp");
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         userService.getUsers().forEach(user -> {
             System.out.println(user.getEmail());
-            System.out.println(user.getPassword());
         });
-        System.out.println(userService==null?"null":"khong null");
-
-        requestDispatcher.forward(request, response);
     }
-
 }
